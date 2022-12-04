4af65ca97cddf73cf0c399cac77ced3b56ef2b2f
yannainghtet <94881139+yannainghtet@users.noreply.github.com>
2022-12-04 12:29:33 +0630

    Update smtirri.py

--- smtirri.py
+++ smtirri.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template , request
 import machinelearning
 app = Flask(__name__)
-server = app.server
 wateramount =0
 @app.route("/", methods = ["GET","POST"])
 def predict():
